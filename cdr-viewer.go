4af386a35726962d7e61119c95313234f54a5d21
Ludwig Prager <ludwig.prager@celp.de>
2021-04-04 23:53:47 +0200

    build

--- cdr-viewer.go
+++ cdr-viewer.go
@@ -178,24 +178,3 @@ func cdrALT(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprintf(w, records[1][1])
 
 }
-
-/*
-func csvReader() {
-
-  // 1. Open the file
-  recordFile, err := os.Open(cdr_csv)
-  if err != nil {
-   fmt.Println("An error encountered ::", err)
-  }
-  // 2. Initialize the reader
-  reader := csv.NewReader(recordFile)
-  // 3. Read all the records
-  records, _ := reader.ReadAll()
-
-  records2 := records[:len(records)-10]
-  // 4. Iterate through the records as you wish
-  fmt.Println(records2)
-
-}
-
-*/
