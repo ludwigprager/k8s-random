1ad9db7a082892e761033e225cdd7cf8edfd5f3c
Ludwig Prager <ludwig.prager@celp.de>
2021-04-05 12:24:03 +0200

    ready after 20 sec

--- liveness.go
+++ liveness.go
@@ -1,5 +1,6 @@
 
 // will reports unhealthy after 15 sec
+// will reports ready     after 20 sec
 
 package main
 
@@ -13,9 +14,19 @@ import (
 func main() {
 	started := time.Now()
 	http.HandleFunc("/readz", func(w http.ResponseWriter, r *http.Request) {
+/*
 		w.WriteHeader(200)
 		data := (time.Since(started)).String()
 		w.Write([]byte(data))
+*/
+		duration := time.Since(started)
+		if duration.Seconds() > 20 {
+			w.WriteHeader(200)
+			w.Write([]byte("ok"))
+		} else {
+			w.WriteHeader(500)
+			w.Write([]byte("error: getting ready"))
+		}
 	})
 	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		duration := time.Since(started)
